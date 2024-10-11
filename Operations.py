ec270a86ff3ed315e6d37efe8198c71f50892586
Jamel144 <jamel144@live.co.uk>
2024-10-11 18:07:16 +0100

    Added more python on operations file

--- Operations.py
+++ Operations.py
@@ -1,4 +1,4 @@
-intenger = 31
+integer = 31
 
 string = "Jamel"
 
@@ -6,12 +6,13 @@ float = 1.6
 
 boolean= True
 
+shopping_list = ["bread", "milk", "juice", "eggs"]
 
-#find length of string len(sting) = 5 
+#find length of string len(sting) = 5  or amount items in a list [2, 3, 54, 32] 
 
 # format string print(f" My name is {string}")
 
-#find index of string string[2] = m 
+#find index of string string[2] returns the character at the index
 
 # Check the type of item type(item)
 
@@ -19,9 +20,21 @@ boolean= True
 
 #change to all lower caps string.lower()
 
+#Make first letter of a word a in a string capital .capitalised()
+
+# .title() makes each of the words after a space a capitial 
+
+# .swapcase() swaps around all cases from lower to upper and vice versa
+
+#Spilt words using .split(", ") it will split the word at the comma thats eneter between the qoutes
+
+#To join words together .join(", ")
+
 #Slice string string[0:3] prints first 4 letters of the string
 
 # reverse characters in a string string[::-1] returns string backwards 
+
+# Return specified sections of a list round a list list[0:2] ([start:stop:set])
 
 # Repeat string repeated_string = string * 3 returns new variable of the string repeated 
 
@@ -29,19 +42,19 @@ boolean= True
 
 # change item to float float(item)
 
-# change item to intenger int(item)
+# change item to integer int(item)
 
 # Total number of variables = total_number_variables = sum([num1, num2, num3]) = Return all variable total.
 
-# round a float or intenger to two decimal places round(intenger, 2)    /or formatted {intenger:.2f}
+# round a float or integer to two decimal places round(integer, 2)    /or formatted {integer:.2f}
 
-# use // when dividing numbers to get an intenger back otherwise it will be a float 
+# use // when dividing numbers to get an integer back otherwise it will be a float 
 
 # To find the modulo (remainder) is 7 % 3 = 1 (7 / 3 is leaves one as a remainder so the modulo is 1) 
     # can also be used to find out if a number is odd or even by using (number % 2) = 0 is even/ = 1 is odd.
 
 # ** isthe exponentiation operator. It is used to raise a number (the base) to the power of another number (the exponent).
-    #so (2**3) (base ** exponent) = 8 (2*2*3) = 8
+    #so (2**3) (base ** exponent) = 8 (2*2*2) = 8
 
     # PEMDAS stands for:
 
@@ -49,6 +62,51 @@ boolean= True
     # E: Exponentiation **
     # MD: Multiplication * and Division / (from left to right)
     # AS: Addition + and Subtraction - (from left to right)
+
+#  The operator on the right is assigned to the equal sign on the left '(+-/*) = '
+#  equal to (==) will give true or false 
+#Not equal to (!=) will give true or false 
+
+#To find a the average of a number you need to sum all numbers together then divide them by the amountof numbers there were.
+
+#min(integer1, integer2) gives the min value out of number and max(integer1, integer2) gives the max value
+
+#Add item to list .append("new item") adds to end of list 
+
+# Add to specific place in list .insert(3, "new item")  < Add item at the index stated
+
+#Return last item on the list list[-1]
+
+# Remove first occurance of an item in a list list.remove("this_item") only removes the first one it gets
+
+# Remove a item in a specified index list.pop(2)  removes this item in that index
+
+# Arrnge list alphabetically or numbered using list.sort()
+
+# Handle list errors
+
+#            try:
+#                print(list[10])
+#            except IndexError:
+#                print("This index is not in the list")
+
+# An example of next list, can obatin certain exact bits of information from it by 
+#   print(nested_list[0][1]) #print numbers [68, 99, 8]
+
+#            nested_list = [
+#                    ["Jim" [68, 99, 8]]
+#                    ["Carl" [5, 7, 10]]
+#                    ["Jenny" [12, 15, 19]]
+#            ]
+
+#
+
+
+
+#                a_dictionary = {"brand": "Ford",
+#                    "model": "Mustang",
+#                    "year": 1964
+#                  }
 
 
 
